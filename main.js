c6c77bbfc77afb0d91dd3559a9ab682569bb85b1
Kaleb Lape <kaleb.lape@metova.com>
2018-03-27 11:23:50 -0500

    Output keyboard events.
    - Add rxjs to simplify operations on the event stream.
    - Adapt node-keylogger for our use-case.

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log('Hello World!')
+const Rx = require('rxjs/Rx')
+
+const Keyboard = require('./vendor/node-keylogger/index')
+const KeyboardEvents = require('./lib/keyboard-events')
+
+run(process.env.KEYBOARD_DEVICE)
+
+function run(device) {
+  console.log(`listening on ${device}`)
+  const keyboard = new KeyboardEvents(device).listen()
+
+  keyboard.on('error', console.error)
+
+  const downEvents = Rx.Observable.fromEvent(keyboard, 'down')
+  const enterEvents = downEvents.filter(event => event.key === 'KEY_ENTER')
+  const lines = downEvents.filter(event => event.key !== 'KEY_ENTER')
+    .buffer(enterEvents)
+
+  lines.map(line => line.map(event => event.key.split('_')[1]).join(''))
+    .forEach(console.log)
+}
