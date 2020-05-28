02274de0d53eea51d1f76bc5860c85f2245c0190
Kaleb Lape <pingortle@gmail.com>
2020-05-28 08:43:46 -0500

    Autodetect usb rfid device on linux.

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const { run } = require('@cycle/run')
 
 const main = require('./lib/main')
+const findDevice = require('./lib/find-device')
+
 const fetchCodes = require('./lib/drivers/fetch-codes')
 const playWithSox = require('./lib/drivers/play-with-sox')
 const log = require('./lib/drivers/log')
 
 run(main, {
-  codes: fetchCodes(process.env.KEYBOARD_DEVICE),
+  codes: fetchCodes(findDevice({ override: process.env.KEYBOARD_DEVICE })),
   play: playWithSox(),
   log: log()
 })
