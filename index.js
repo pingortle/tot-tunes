6b7bb2c724e67a34316d0369efd164c1130da5e4
Kaleb Lape <pingortle@gmail.com>
2020-06-01 18:59:23 -0500

    Add PoC service to show last tapped tape.

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ const findDevice = require('./lib/find-device')
 const fetchCodes = require('./lib/drivers/fetch-codes')
 const playWithSox = require('./lib/drivers/play-with-sox')
 const log = require('./lib/drivers/log')
+const server = require('./lib/drivers/server')
+const quitter = require('./lib/drivers/quitter')
 
 run(main, {
   codes: fetchCodes(findDevice({ override: process.env.KEYBOARD_DEVICE })),
   play: playWithSox(),
-  log: log()
+  log: log(),
+  server: server(),
+  quit: quitter()
 })
